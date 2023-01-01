44b438e67f6c113c364adc5e260336f2900692e4
Sherydan <tobar.luis.c@gmail.com>
2023-01-01 00:11:42 -0300

    fix: added try catch statements to endpoints, moved asset-manifest.json file into public

--- index.js
+++ index.js
@@ -1,45 +1,45 @@
 const express = require("express");
 const app = express();
-const cors = require('cors');
-var morgan = require('morgan')
+const cors = require("cors");
+var morgan = require("morgan");
 
-const CsbInspector = require('csb-inspector')
-CsbInspector()
+const CsbInspector = require("csb-inspector");
+CsbInspector();
 
-const {getPosts, addPost} = require('./models/querys')
-const {validateInput} = require("./helpers/validation")
+const { getPosts, addPost } = require("./models/querys");
+const { validateInput } = require("./helpers/validation");
 
-app.use(express.static('public'))
-app.use(cors())
+app.use(express.static("public"));
+app.use(cors());
 app.use(express.json());
 
-app.use(morgan('tiny'))
+app.use(morgan("tiny"));
 
 app.listen(3000, console.log("¡Servidor encendido!"));
 
-
-app.get("/", (req, res) =>{
+app.get("/", (req, res) => {
     try {
-        return res.sendFile(__dirname + "/public/index.html")
+        return res.sendFile(__dirname + "/public/index.html");
     } catch (error) {
-        res.status(404).send(error)
+        res.status(404).send(error);
     }
-})
+});
 
 app.get("/posts", async (req, res) => {
-    const posts = await getPosts();
-    res.json(posts)
-})
-
-app.post("/posts", async (req,res) => {
-    const payload = req.body
-    if (!validateInput(payload)) {
-        res.status(400).send("all fields are required")
-        return
-    } else {
-        await addPost(payload)
+    try {
+        const posts = await getPosts();
+        res.json(posts);
+    } catch (error) {
+        res.status(404).send('internal server error')
     }
-    
-})
+});
 
-
+app.post("/posts", async (req, res) => {
+    const payload = req.body;
+    if (!validateInput(payload)) {
+        res.status(400).send("all fields are required");
+        return;
+    } else {
+        await addPost(payload);
+    }
+});
