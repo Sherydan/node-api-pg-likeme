0e3fcc6baf56e4f1c0e3acdf6348cdd0068e64a1
Sherydan <tobar.luis.c@gmail.com>
2023-01-01 00:02:22 -0300

    feat:added validation into querys

--- models/querys.js
+++ models/querys.js
@@ -1,11 +1,11 @@
-const {Pool} = require("pg");
-require('dotenv').config()
+const { Pool } = require("pg");
+require("dotenv").config();
 const pool = new Pool({
     host: process.env.HOST,
     database: process.env.DATABASE,
     user: process.env.USER,
-    password: process.env.PASSWORD
-})
+    password: process.env.PASSWORD,
+});
 
 const getDate = async () => {
     const result = await pool.query("SELECT NOW()");
@@ -13,15 +13,25 @@ const getDate = async () => {
 };
 
 const getPosts = async () => {
-    const {rows} = await pool.query("SELECT * FROM posts")
-    return rows
-}
+    try {
+        const { rows } = await pool.query("SELECT * FROM posts");
+        return rows;
+    } catch (error) {
+        console.log("Error while getting posts");
+        return error;
+    }
+};
 
 const addPost = async (payload) => {
-    const consulta = "INSERT INTO posts VALUES (DEFAULT, $1, $2, $3, $4)"
-    const values = [payload.titulo, payload.url, payload.descripcion, 0]
-    const result = await pool.query(consulta, values)
-    return result.rows
-}
+    try {
+        const consulta = "INSERT INTO posts VALUES (DEFAULT, $1, $2, $3, $4)";
+        const values = [payload.titulo, payload.url, payload.descripcion, 0];
+        const result = await pool.query(consulta, values);
+        return result.rows;
+    } catch (error) {
+        console.log("Error adding post");
+        return error
+    }
+};
 
-module.exports = {getPosts, addPost}
+module.exports = { getPosts, addPost };
