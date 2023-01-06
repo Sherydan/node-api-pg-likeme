58de854963c076778ecece64d5179bf8c9e2decf
Sherydan <tobar.luis.c@gmail.com>
2023-01-06 17:42:21 -0300

    added singleton db connection

--- models/querys.js
+++ models/querys.js
@@ -1,16 +1,4 @@
-const { Pool } = require("pg");
-require("dotenv").config();
-const pool = new Pool({
-    host: process.env.HOST,
-    database: process.env.DATABASE,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-});
-
-const getDate = async () => {
-    const result = await pool.query("SELECT NOW()");
-    console.log(result);
-};
+const pool = require("../helpers/connectionDb").getInstance();
 
 const getPosts = async () => {
     try {
